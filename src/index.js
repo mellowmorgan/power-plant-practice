ca318a9d854269d1e3c49323a50010ffb40ba4f2
mellowmorgan <morgan.waites@gmail.com>
2022-02-01 15:22:15 -0800

    add plant states stuff
    Co-Authored-By: Kate Kiatsiri <keidsiri@gmail.com>
    Co-authored-by: Karen Axon <kaxonwa@gmail.com>
    Co-Authored-By: Curtis Brooks <curtisbrooks678@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import $ from 'jquery';
+import $, { type } from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
@@ -16,37 +16,66 @@ const storeState = () => {
 
 const stateControl = storeState();
 
-const changeState = (prop) => {
-  return (value) => {
-    return (state) => ({
-    ...state,
-    [prop] : (state[prop] || 0) + value
-    })
-  }
-}
+// const changeState = (prop) => {
+//   return (value) => {
+//     return (state) => ({
+//     ...state,
+//     [prop] : (state[prop] || 0) + value
+//     })
+//   }
+// }
+const changeState = (valueName) => {
+  return (prop)=>{
+    return (value) => {
+      return (state) => ({
+      ...state,
+      [prop] : (state[prop] || 0) + value,
+      "name" : valueName || state["name"]
+      })
+    }
+}}
 
-const feed = changeState("soil")(1);
-const blueFood = changeState("soil")(5);
-const redFood = changeState("soil")(-2);
 
-// const hydrate = changeState("water")(1);
+
+  
+// const feed = changeState("soil")(1);
+const blueFood = changeState("")("soil")(5);
+
+const hydrate = changeState("")("water")(1);
 // const superWater = changeState("water")(5);
+
+const nutrient = changeState("")("fertilizer")(1);
 
 
 $(document).ready(function(){
+  // event.preventDefault();
+  $('#type').click(function(){
+    const input = $("#input-type").val();
+    const plantName = changeState(input)("")(0);
+    const newState = stateControl(plantName);
+    $('#type-value').text(`${newState.name}`);
+  }); 
+  
   $('#feed').click(function(){
-    const newState = stateControl(feed);
-    const newState1 = stateControl(blueFood);
-    const newState2 = stateControl(redFood);
-    $('#soil-value').text(`Soil: ${newState.soil}`);
-    $('#soil-value').text(`Soil: ${newState1.soil}`);
-    $('#soil-value').text(`Soil: ${newState2.soil}`);
+    const newState = stateControl(blueFood);
+    $('#soil-value').text(`${newState.soil}`);
+
   });
 
-  $('#show-state').click(function() {
-    const currentState = stateControl();
-    $('#soil-value').text(`Soil: ${currentState.soil}`);
+  $('#water').click(function(){
+    const newState = stateControl(hydrate);
+    $('#water-value').text(`${newState.water}`);
   });
+
+  $('#fertilizer').click(function(){
+    const newState = stateControl(nutrient);
+    $('#fertilizer-value').text(`${newState.fertilizer}`);
+  });
+
+  // $('#show-state').click(function() {
+  //   const currentState = stateControl();
+  //   $('#soil-value').text(`${currentState.soil}`);
+  // });
 
 });
 
